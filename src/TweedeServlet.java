29b13ba12566d2e5f3e307edf92c408e5ab009ef
KevinVanbockryrk <maintainer@vanbockryck.net>
2013-02-21 15:00:38 +0100

    Probeer de tweede Servlet!

--- src/TweedeServlet.java
+++ src/TweedeServlet.java
@@ -25,9 +25,23 @@ public class TweedeServlet extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		java.util.Date today = new java.util.Date();
-		out.println("<html> " + "<body>" + "<h1>Een Tweede Servlet</h1>" + "<br>" + today + "<h2>Bekijk de DD van dit project ! (kevin)<h2>" + "</body>" + "</html>");
+		out.println("<html> <body>" + "<h1>Een Tweede Servlet</h1>" + "<br>" + today + "<h2>Bekijk de DD van dit project ! (kevin)<h2>" 
+		
+				+ "<form name=\"input\" action=\"TweedeServlet\" method=\"POST\"><input type=\"text\" name=\"test\"/> <input type=\"submit\"/></form>" + "</body>" + "</html>"
+				
+				);
 
 		
+	}
+	
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		String test = request.getParameter("test");
+		
+		out.println( "<html> <body>" + test + "</body> </html>");
+	
 	}
 
 }
