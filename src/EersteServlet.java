14d18824cad3d6b56a1f6d5df67ebf464e382485
Wouter Wilms <wilmswouter@gmail.com>
2013-02-21 14:27:20 +0100

    Laatste changes

--- src/EersteServlet.java
+++ src/EersteServlet.java
@@ -15,7 +15,7 @@ public class EersteServlet extends HttpServlet {
 		
 		 PrintWriter out = response.getWriter();
 		 java.util.Date today = new java.util.Date();
-		 out.println("<html> " + "<body>" + "<h1>Een Eerste Servlet</h1>" + "<br>" + today + "<h2>Bekijk de DD van dit project ! (kevin) Wouter is de beste<h2>" + "</body>" + "</html>");
+		 out.println("<html> " + "<body>" + "<h1>Een Eerste Servlet</h1>" + "<br>" + today + "<h2>Bekijk de DD van dit Kevin... :-) ! (kevin) Wouter is de beste<h2>" + "</body>" + "</html>");
 		
 	}
 	public static void main(String[] args) {
