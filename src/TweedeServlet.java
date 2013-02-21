651d8c205a88b7254b3c842b74e5c0b820cb13a7
Kevin Vanbockryck <maintainer@vanbockryck.net>
2013-02-21 13:36:01 +0100

    Tweede Servlet toegevoed

---
+++ src/TweedeServlet.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class TweedeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public TweedeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		java.util.Date today = new java.util.Date();
+		out.println("<html> " + "<body>" + "<h1>Een Tweede Servlet</h1>" + "<br>" + today + "<h2>Bekijk de DD van dit project ! (kevin)<h2>" + "</body>" + "</html>");
+
+		
+	}
+
+}
